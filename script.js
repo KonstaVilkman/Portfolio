d62c6759a9e4f949d3e0b1830fe760e2a204b20e
Konsta <konsta.vilkman@edu.salpaus.fi>
2020-11-10 11:27:04 +0000

    20201110

---
+++ script.js
@@ -0,0 +1,13 @@
+
+// Toteuuttaa openNavin (avaa navigaation kun siitä painetaan)
+function openNav() {
+  document.getElementById("mySidenav").style.width = "250px";
+}
+// Sulkee valikon X kohdalta
+function closeNav() {
+  document.getElementById("mySidenav").style.width = "0";
+}
+
+
+// ---------------------PROJEKTI 3!------------------------//
+console
