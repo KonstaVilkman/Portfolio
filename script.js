bdeed1387b9895e151bdfc4c63b59019922b67eb
Konsta <konsta.vilkman@edu.salpaus.fi>
2020-11-17 10:51:48 +0000

    20201117.

--- script.js
+++ script.js
@@ -14,3 +14,7 @@ function myFunction() {
    var element = document.body;
    element.classList.toggle("nightmode");
 }
+function myyFunction() {
+   var element = document.body;
+   element.classList.toggle("rainbow");
+}
